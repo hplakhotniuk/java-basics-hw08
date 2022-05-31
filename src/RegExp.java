f9960f4ed2392d653b5fa1ff9682356f5eac9578
Heorhii Plakhotniuk <metalhrt7@gmail.com>
2022-05-31 22:16:22 +0300

    HW10

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,23 +1,24 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 /**
  * Regular expressions.
  */
 public class RegExp {
 
-    /**
-     * Get IP address from text.
-     *
-     * @param text text to parse
-     * @return String with IP address
-     */
-    public static String getIpAddress(String text) {
-        String ipAddress = "[0-2]?[0-9]{0,2}[.][0-2]?[0-9]{0,2}[.][0-2]?([0-5]{0,2}|8)[.][0-2]?([0-9]{0,2})";
-        Pattern pattern = Pattern.compile(ipAddress);
-        Matcher matcher = pattern.matcher(text);
-        while (matcher.find()) {
-            return matcher.group();
-        }
-        return "";
+  /**
+   * Get IP address from text.
+   *
+   * @param text text to parse
+   * @return String with IP address
+   */
+  public static String getIpAddress(String text) {
+    String ipAddress = "[0-2]?[0-9]{0,2}[.][0-2]?[0-9]{0,2}[.][0-2]?([0-5]{0,2}|8)[.][0-2]?([0-9]{0,2})";
+    Pattern pattern = Pattern.compile(ipAddress);
+    Matcher matcher = pattern.matcher(text);
+    while (matcher.find()) {
+      return matcher.group();
     }
+    return "";
+  }
 }
